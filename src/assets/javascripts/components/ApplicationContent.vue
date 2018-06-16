530230fe2be71d4cc7715f8a52954ff30231d90e
taplar <taplarhw@gmail.com>
2018-06-16 09:28:21 -0500

    Created the initial build container component

--- src/assets/javascripts/components/ApplicationContent.vue
+++ src/assets/javascripts/components/ApplicationContent.vue
@@ -1,15 +1,18 @@
 <template>
 	<div class="content row position-absolute z-index-100">
 		<disclaimer></disclaimer>
+		<build-container></build-container>
 	</div>
 </template>
 
 <script>
+	import BuildContainer from '../components/BuildContainer.vue';
 	import Disclaimer from '../components/Disclaimer.vue';
 
 	export default {
 		components: {
-			Disclaimer
+			BuildContainer
+			, Disclaimer
 		}
 	}
 </script>
---
+++ src/assets/javascripts/components/BuildContainer.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="build-container">
+        <span style="color:white;">This is a build container</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		components: {
+		}
+	}
+</script>
